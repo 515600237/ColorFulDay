63d7e0bdc6491ce8fd9953b7ad8d2969bcacb3ee
yangzhi <515600237@qq.com>
2021-02-13 19:24:25 +0800

    '新名字'

---
+++ my-project/src/views/Example.vue
@@ -0,0 +1,15 @@
+<template>
+            <div class="example">
+                    这是例子
+            </div>
+</template>
+
+<script>
+    export default {
+        name: "Example"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ my-project/src/views/Test.vue
@@ -0,0 +1,35 @@
+<template>
+            <div class="test">
+                    这是test
+            </div>
+</template>
+
+<script>
+    export default {
+        name: "Test",
+        data(){
+            return {
+                data:{
+                    list:[],
+                    ruleForm:{
+
+                    },
+                    pageInfo:{
+                        current:1,
+                        total:100,
+                        size:10,
+                    }
+                },
+
+            }
+        },
+        methods:{
+
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
